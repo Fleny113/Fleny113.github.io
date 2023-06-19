82ddce96ce18577a6500a073259cf3960b2c480b
Fleny <fleny113@outlook.com>
2023-06-19 18:54:10 +0200

    initial work

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <main>
+        <h1>Hi</h1>
+    </main>
 </template>
+
+<style scoped>
+main {
+    width: 600px;
+    height: 300px;
+
+    background-color: hsla(0 0% 84% / 0.125);
+    backdrop-filter: blur(4px);
+
+    border: 1px solid hsla(0 0% 98% / 0.125);
+    border-radius: 25px;
+}
+
+h1 {
+    text-align: center;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/style.css"],
   devtools: {
     enabled: true,
   },
