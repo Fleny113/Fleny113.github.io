7b7f630da49eb8c25364ef74e292028b9194a657
Fleny <fleny113@outlook.com>
2026-06-20 12:43:30 +0000

    Add initial content
    Co-Authored-By: TheTinkerersHaven <93077613+TheTinkerersHaven@users.noreply.github.com>

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,20 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: "Fira Code",
+            cssVariable: "--font-fira-code",
+        },
+        {
+            provider: fontProviders.google(),
+            name: "Zalando Sans SemiExpanded",
+            cssVariable: "--font-zalando-sans",
+        }
+    ],
+    experimental: {
+        chromeDevtoolsWorkspace: true,
+    }
+});
