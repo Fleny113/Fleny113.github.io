432b2c5e81509f28c31217248fa95e174801bd6d
Fleny <fleny113@outlook.com>
2026-06-26 12:00:44 +0200

    Add projects

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,6 @@ export function getRandomElement<T>(arr: T[]): T {
 export function assert(condition: unknown, message?: string): asserts condition {
     if (!condition) {
         const error = new Error(message);
-        // @ts-expect-error Typescript puts this method on the @types/node package, but it is actually available in the browser as well.
         // We do ?. just in case some browsers don't support it, but it should be fine in modern browsers.
         Error.captureStackTrace?.(error, assert);
         throw error;
